4ead0a27f29f58ae0332b03c5926e485a0fd1332
maciejdybowski <m_dybowski@onet.pl>
2020-09-22 07:42:49 +0200

    add favicon and change font size

--- src/components/VideoCard.js
+++ src/components/VideoCard.js
@@ -5,7 +5,7 @@ import ThumbUpIcon from '@material-ui/icons/ThumbUp';
 
 const base_url = "https://image.tmdb.org/t/p/original";
 
-const VideoCard = forwardRef(({ movie },ref) => {
+const VideoCard = forwardRef(({ movie }, ref) => {
 
     return (
         <div ref={ref} className='videoCard'>
@@ -20,7 +20,7 @@ const VideoCard = forwardRef(({ movie },ref) => {
             <h2>{movie.title || movie.original_name}</h2>
             <p className='videoCard__stats'>
                 {movie.release_date || movie.first_air_date}
-                <ThumbUpIcon />{movie.vote_count}</p>
+                <ThumbUpIcon /> {movie.vote_count}</p>
         </div>
     )
 }
